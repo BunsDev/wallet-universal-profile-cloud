40f251ebd5a31ed6cd3d3ba39000474f51a4780e
Hugoo <git@hugom.xyz>
2022-08-18 11:29:29 +0000

    Deploying to gh-pages from @ lukso-network/wallet.universalprofile.cloud@557b2c509d5caf4d136c3f9af428c3b0c098cecf 🚀

--- _next/static/bulM5pcbEj8riGIexRCnP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e7df2e6d25206b5b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
